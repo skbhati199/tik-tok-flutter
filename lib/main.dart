e0fc0e95515b888ba6f867c97101f87a55421999
Sonu Kumar from Mac OS <skbhati199@gmail.com>
2019-04-19 17:44:21 +0530

    initial project

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+// This example shows a [Scaffold] with an [AppBar], a [BottomAppBar] and a
+// [FloatingActionButton]. The [body] is a [Text] placed in a [Center] in order
+// to center the text within the [Scaffold] and the [FloatingActionButton] is
+// centered and docked within the [BottomAppBar] using
+// [FloatingActionButtonLocation.centerDocked]. The [FloatingActionButton] is
+// connected to a callback that increments a counter.
+
+import 'package:flutter/material.dart';
+import 'package:pippop/home.dart';
+//pippop
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // Uncomment in phase 3 to apply white to text
+        textTheme: Theme.of(context)
+            .textTheme
+            .apply(bodyColor: Colors.white, displayColor: Colors.white),
+      ),
+      home: Home(),
+    );
+  }
+}
